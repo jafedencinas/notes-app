e3f4d3ed7b90c6d3f9c5f2e9297065fd29d695a4
Jafed Encinas <60824778+jafedencinas@users.noreply.github.com>
2021-03-25 21:45:23 -0600

    3rd commit from VSCode

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: "",component: NotesListComponent }
+  { path: "",component: MainLayoutComponent, children: [
+    { path: "",component: NotesListComponent  }
+  ] }
 ];
 
 @NgModule({
