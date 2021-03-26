21b48195783cfe99821bddb93217df2fe7b256c6
Jafed Encinas <60824778+jafedencinas@users.noreply.github.com>
2021-03-25 22:16:57 -0600

    FrontEnd mockup completed

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesListComponent implements OnInit {
 
+  cardTitle: string = "abc";
+
+
   constructor() { }
 
   ngOnInit(): void {
